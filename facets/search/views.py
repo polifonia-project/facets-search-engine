8e954a3ae9908d1665be3b121ea249da5797b617
Raphaël Fournier-S'niehotta <raphael@fournier-sniehotta.fr>
2022-05-04 17:37:40 +0200

    nice first UI, which works. needs more tests, though

--- facets/search/views.py
+++ facets/search/views.py
@@ -2,29 +2,73 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.template import loader
+import json
+
+from elasticsearch import Elasticsearch
+
+from lib.process import ScoreProcessing
+from lib.search.IndexWrapper import IndexWrapper
+from lib.search.SearchContext import *
+
 
 from elasticsearch import Elasticsearch
 
 # Create your views here.
 
 def index(request):
+    es = Elasticsearch()
+    indices = es.indices.get_alias().keys()
+    context = {"indices_names": indices}
+
     template = loader.get_template('search/index.html')
-    context = {}
     return HttpResponse(template.render(context, request))
 
 @csrf_exempt
 def results(request):
     # print("result view called")
+    matching_doc_ids = {}
+    es = Elasticsearch()
+    indices = es.indices.get_alias().keys()
+
     if request.method == 'POST':
         try:
-            pattern = request.POST.get('pattern', False)
-            mirror = request.POST.get('mirror', False)
-            search_type = request.POST.get('searchtype', False)
-            if pattern:
-                print(pattern,mirror,search_type)
-                results = {}
-        except: 
-            pass
+            searchinput = {}
+            searchinput["pattern"] = request.POST.get('pattern', False)
+            if request.POST.get('mirror', False):
+                searchinput["mirror"] = True
+            else:
+                searchinput["mirror"] = False
+            searchinput["type"] = request.POST.get('searchtype', False)
+            searchinput["index_name"] = request.POST.get('indexname', False)
+            if searchinput["pattern"]:
+                print(searchinput)
+                searchcontext = SearchContext()
+                # json -> searchcontext
+                searchcontext.read(searchinput)
+                # print the content of SearchContext object
+                print("\n\nsearch type: ", searchcontext.search_type)
+                if searchcontext.pattern:
+                    print("ABC encoded pattern: ", searchcontext.pattern)
+                if searchcontext.text:
+                    print("text: ", searchcontext.text)
+                if searchcontext.search_type != "lyrics":
+                    print("mirror search: ", searchcontext.mirror_search,"\n\n")
+
+                index_wrapper = IndexWrapper(searchinput["index_name"])
+                # ES returns a "response" object with all the documents that matches query
+                matching_docs = index_wrapper.search(searchcontext)
+
+                # Get a list of doc_id
+                matching_doc_ids = {}
+                for hit in matching_docs.hits.hits:
+                    matching_doc_ids[hit['_id']] = 1
+
+                print("Matching documents are:", matching_doc_ids)
+        except:
+            print("error")
     template = loader.get_template('search/results.html')
-    context = {"pattern": pattern, "results": results}
+    context = {
+        "searchinput": searchinput,
+        "results": matching_doc_ids,
+        "indices_names": indices}
     return HttpResponse(template.render(context, request))
