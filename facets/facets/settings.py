5208b70848ba4f792ffb93a916e8c755a24243b5
TiangeZhu <tiangezhu@outlook.com>
2023-10-20 03:17:44 +0200

    reverse fix

--- facets/facets/settings.py
+++ facets/facets/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # From Neuma:
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
-# MEDIA_ROOT = BASE_DIR.as_posix()+ "/media"
+#MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = BASE_DIR.as_posix()+ "/media"
 
 
 MEDIA_URL = '/media/'
